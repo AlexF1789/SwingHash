6e872c7b1fbe8eb16e0f431f5c37fe759e44cfbe
AlexF1789 <al.flora@outlook.it>
2026-07-14 14:02:25 +0200

    Completed history frame
    (feat): completed history frame

--- src/main/java/net/alexf1789/swinghash/frames/HistoryFrame.java
+++ src/main/java/net/alexf1789/swinghash/frames/HistoryFrame.java
@@ -20,10 +20,34 @@ public class HistoryFrame extends JFrame {
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 
         // let's set the layout and the elements
-        setLayout(new BorderLayout());
+        setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
 
+        gbc.insets = new Insets(15, 15, 15, 15);
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+
+        gbc.weightx = 1.0;
+        gbc.weighty = 0.9;
+        gbc.gridwidth = 3;
         JScrollPane scrollPane = new JScrollPane(table);
-        add(scrollPane, BorderLayout.CENTER);
+        add(scrollPane, gbc);
+
+        gbc.gridy++;
+        gbc.weighty = 0.1;
+        gbc.weightx = 0.5;
+        gbc.gridwidth = 1;
+        JButton clearButton = new JButton("Clear");
+        clearButton.addActionListener(e -> clearHistory());
+
+        add(clearButton, gbc);
+
+        gbc.gridx = 2;
+        JButton closeButton = new JButton("Close");
+        closeButton.addActionListener(e -> dispose());
+        add(closeButton, gbc);
 
         // let's show the window
         pack();
@@ -34,5 +58,26 @@ public class HistoryFrame extends JFrame {
         HistoryTable historyTable = new HistoryTable(settings);
         table = new JTable(historyTable.getEntries(), historyTable.getColumns());
     }
+
+    private void clearHistory() {
+        int result = JOptionPane.showConfirmDialog(
+            this,
+            "By pressing yes you will permanently delete the history.",
+            "Are you sure?",
+            JOptionPane.YES_NO_OPTION
+        );
+
+        if(result == 0) {
+            settings.clearHistory();
+            settings.save();
+            reloadWindow();
+        }
+    }
+
+    private void reloadWindow() {
+        SwingUtilities.invokeLater(() -> new HistoryFrame(Settings.reloadSettings()));
+        dispose();
+    }
+
 
 }
